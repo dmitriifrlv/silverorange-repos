3eb659faa5e80d5d14019d939a175d310e08ab47
Dmitrii Frolov <65853205+dmitriifrlv@users.noreply.github.com>
2022-05-18 19:00:52 -0400

    added style normalization

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import { App } from './App';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import CssBaseline from '@mui/material/CssBaseline';
 
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <CssBaseline />
       <App />
     </Provider>
   </React.StrictMode>,
