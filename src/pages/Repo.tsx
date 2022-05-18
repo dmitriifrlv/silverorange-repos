8fe91cac5e4d2e171e1f1ac59ecc1335e6a83ff4
Dmitrii Frolov <65853205+dmitriifrlv@users.noreply.github.com>
2022-05-18 16:43:26 -0400

    added fetching data for selected repo

--- src/pages/Repo.tsx
+++ src/pages/Repo.tsx
@@ -1,7 +1,17 @@
 import { useParams } from 'react-router-dom';
+import { useGetRepoByIdQuery } from '../services/service';
 
 export function Repo() {
   const { repoId } = useParams();
+  const { isLoading, isError, isSuccess, data } = useGetRepoByIdQuery(
+    repoId as string
+  );
 
-  return <p>{repoId}</p>;
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (isError) {
+    return <p>Something went wrong</p>;
+  }
+  return repoId && isSuccess && data ? <p>{data.name}</p> : null;
 }
