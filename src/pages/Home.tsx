5d5a24cdbb64f23824a18b46f1171c6bb6b4e2b5
Dmitrii Frolov <65853205+dmitriifrlv@users.noreply.github.com>
2022-05-18 15:45:24 -0400

    added fetching repos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,24 @@
+import { useGetAllReposQuery } from '../services/service';
+import { useNavigate } from 'react-router-dom';
+
 export function Home() {
-  return <p>Repos</p>;
+  const navigate = useNavigate();
+  const { isLoading, isError, isSuccess, data } = useGetAllReposQuery();
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (isError) {
+    return <p>Something went wrong</p>;
+  }
+  return isSuccess && data ? (
+    <ul>
+      {data.map((item) => {
+        return (
+          <li key={item.id} onClick={() => navigate(`${item.id}`)}>
+            {item.name}
+          </li>
+        );
+      })}
+    </ul>
+  ) : null;
 }
