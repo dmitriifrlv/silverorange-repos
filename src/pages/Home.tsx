624159ddd140181424718afcbc9e2f9bef626b0c
Dmitrii Frolov <65853205+dmitriifrlv@users.noreply.github.com>
2022-05-18 16:07:50 -0400

    added sorted list of repos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,33 @@
 import { useGetAllReposQuery } from '../services/service';
 import { useNavigate } from 'react-router-dom';
+import { Repo } from '../models/Repo';
 
 export function Home() {
   const navigate = useNavigate();
   const { isLoading, isError, isSuccess, data } = useGetAllReposQuery();
+
   if (isLoading) {
     return <p>Loading...</p>;
   }
   if (isError) {
     return <p>Something went wrong</p>;
   }
-  return isSuccess && data ? (
+  return data && isSuccess ? (
     <ul>
-      {data.map((item) => {
-        return (
-          <li key={item.id} onClick={() => navigate(`${item.id}`)}>
-            {item.name}
-          </li>
-        );
-      })}
+      {JSON.parse(JSON.stringify(data))
+        .sort(function (a: Repo, b: Repo) {
+          return (
+            new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+          );
+        })
+        .map((repo: Repo) => {
+          return (
+            <li key={repo.id} onClick={() => navigate(`${repo.id}`)}>
+              {repo.name} {repo.description} {repo.language} {repo.forks_count}{' '}
+              *{repo.created_at}*
+            </li>
+          );
+        })}
     </ul>
   ) : null;
 }
