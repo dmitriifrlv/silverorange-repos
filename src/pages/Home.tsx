53e24cb48bf6602fe825329dad4b34f12c1cccf9
Dmitrii Frolov <65853205+dmitriifrlv@users.noreply.github.com>
2022-05-18 19:01:12 -0400

    added styles and mui components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,29 +2,68 @@ import { useState } from 'react';
 import { useGetAllReposQuery } from '../services/service';
 import { useNavigate } from 'react-router-dom';
 import { Repo } from '../models/Repo';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import { Typography } from '@mui/material';
+import styled from '@emotion/styled';
+import { Loader } from '../components/Loader';
+
+const APP_CONTAINER = styled.main`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
 
 export function Home() {
   const navigate = useNavigate();
   const { isLoading, isError, isSuccess, data } = useGetAllReposQuery();
   const [filter, setFilter] = useState<string | null>(null);
 
+  const handleFilter = (
+    event: React.MouseEvent<HTMLElement>,
+    language: string | null
+  ) => {
+    setFilter(language);
+  };
+
   if (isLoading) {
-    return <p>Loading...</p>;
+    return <Loader />;
   }
   if (isError) {
     return <p>Something went wrong</p>;
   }
 
   return data && isSuccess ? (
-    <>
+    <APP_CONTAINER>
+      <Typography align="center" variant="h4">
+        List of available repositories
+      </Typography>
       <div>
-        {[...new Set(data.map((item) => item.language))].map((language) => (
-          <button onClick={() => setFilter(language)} key={language}>
-            {language}
-          </button>
-        ))}
+        <ToggleButtonGroup
+          value={filter}
+          exclusive={true}
+          onChange={handleFilter}
+          aria-label="language filter"
+        >
+          {[...new Set(data.map((item) => item.language))].map((language) => (
+            <ToggleButton
+              key={language}
+              value={language}
+              aria-label="left aligned"
+              disableRipple={true}
+              size="small"
+            >
+              {language}
+            </ToggleButton>
+          ))}
+        </ToggleButtonGroup>
       </div>
-      <ul>
+
+      <List>
         {JSON.parse(JSON.stringify(data))
           .sort(function (a: Repo, b: Repo) {
             return (
@@ -35,13 +74,22 @@ export function Home() {
           .filter((repo: Repo) => (filter ? repo.language === filter : true))
           .map((repo: Repo) => {
             return (
-              <li key={repo.id} onClick={() => navigate(`${repo.id}`)}>
-                {repo.name} {repo.description} {repo.language}{' '}
-                {repo.forks_count} *{repo.created_at}*
-              </li>
+              <ListItem key={repo.id} disablePadding={true}>
+                <ListItemButton onClick={() => navigate(`${repo.id}`)}>
+                  <ListItemText
+                    primary={`${repo.name} (${repo.language})
+                 `}
+                    secondary={`${repo.description ?? ''} ${
+                      repo.description ? ',' : ''
+                    } forked ${repo.forks_count} ${
+                      repo.forks_count === 1 ? 'time' : 'times'
+                    } `}
+                  />
+                </ListItemButton>
+              </ListItem>
             );
           })}
-      </ul>
-    </>
+      </List>
+    </APP_CONTAINER>
   ) : null;
 }
